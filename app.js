2f39d0ea0157da12f6bbf78742716ba14773a2dd
Sifan Geremu <wins33975@gmail.com>
2026-09-04 17:03:39 +0300

    Set up Express server and add dotenv dependency

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("server running buddy")
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+
+app.get('/',(req,res)=>{
+res.send("This is a subscription tracker api")
+});
+
+const PORT= process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`port is running on ${PORT}`);
+});
+
+export default app;
