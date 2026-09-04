fd4e66d147be8605bfbec38dce5aa65c4274924f
Sifan Geremu <wins33975@gmail.com>
2026-09-04 18:05:24 +0300

    Add subscription route

---
+++ routes/subscription.routes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const SubscriptionRouter = Roter();
+
+SubscriptionRouter.get('/',(req,))
---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+
+const userRouter = Router();
+
+userRouter.get('/users',(req,res)=>{});
+userRouter.get('/users',(req,res)=>{});
+userRouter.get('/users',(req,res)=>{});
+userRouter.get('/users',(req,res)=>{});
+userRouter.get('/users',(req,res)=>{});
+
+export default userRouter;
